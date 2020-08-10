05056369190f526272c3f6b7411338ea634ed439
José Clodoalves <jrjcsj@gmail.com>
2020-08-09 21:32:55 -0300

    Criacao de console application com .net core

---
+++ 1-introducao-net-core/GameTOP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+Extensoes recomendadas para programacao .NET no Visual Studio Code:
+- C#
+- C# Extensions
+- Bracket Pair Colorizer
+- Material Icon Theme
+- Path Intellisense
+- Nuget Package Manager
+*/
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new Jogo("Jose");
+
+            jogo.IniciarJogo();
+        }
+    }
+}
+
+class Jogo
+{
+  public string _nomeJogador { get; }
+
+  public Jogo(string nomeJogador)
+  {
+    _nomeJogador = nomeJogador;
+  }
+
+  public void IniciarJogo()
+  {
+    Console.WriteLine($"{_nomeJogador} deu um passe");
+  }
+}
